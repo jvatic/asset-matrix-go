627d98d79c217765b805a5f963e8abda8d01a66f
Jesse Stuart <jesse@jessestuart.ca>
2013-11-19 13:35:35 -0500

    Add package prefix to error

--- matrix/asset_directive.go
+++ matrix/asset_directive.go
@@ -14,7 +14,7 @@ func NewAssetDirective(str string) (*AssetDirective, error) {
 	// parse name and value
 	match := directiveRegex.FindAllStringSubmatch(str, -1)
 	if len(match) < 1 || len(match[0]) < 3 {
-		return nil, fmt.Errorf("Invalid directive string: %s", str)
+		return nil, fmt.Errorf("matrix: invalid directive string: %s", str)
 	}
 
 	return &AssetDirective{String: str, Name: match[0][1], Value: match[0][2]}, nil
