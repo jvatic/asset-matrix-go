23bba66093b14d591b0df5debe1e803a2072d715
Jesse Stuart <jesse@jessestuart.ca>
2015-08-19 12:41:12 -0500

    pin npm dependency versions
    Signed-off-by: Jesse Stuart <jesse@jessestuart.ca>

--- matrix.go
+++ matrix.go
@@ -186,7 +186,7 @@ func installNpmPackages(names []string) error {
 }
 
 func (m *Matrix) installDeps() error {
-	return installNpmPackages([]string{"recast", "es6-promise", "node-sass", "react-tools"})
+	return installNpmPackages([]string{"recast@0.10.30", "es6-promise@3.0.2", "node-sass@3.2.0", "react-tools@0.13.3"})
 }
 
 func (m *Matrix) createTempfiles() error {
