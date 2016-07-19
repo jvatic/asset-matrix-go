af57705bfc48fd06198f5b00ba3bc6daf009e411
Jesse Stuart <jesse@jessestuart.ca>
2016-07-19 16:16:53 -0500

    Bump node-sass
    Signed-off-by: Jesse Stuart <jesse@jessestuart.ca>

--- matrix.go
+++ matrix.go
@@ -203,7 +203,7 @@ func (m *Matrix) installDeps() error {
 	return installNpmPackages([]string{
 		"recast@0.10.30",
 		"es6-promise@3.0.2",
-		"node-sass@3.2.0",
+		"node-sass@3.8.0",
 		"react-tools@0.13.3",
 		"eslint@1.6.0",
 		"eslint-plugin-react@3.5.1",
